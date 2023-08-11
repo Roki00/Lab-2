cfe8836414c671409992853477267fa23634f66b
Eron <eb47836@ubt-uni.net>
2023-08-11 02:15:32 +0200

    shfaqja produkteve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from "./components/header"
 import Footer from "./components/footer"
+import Home from './screens/home'
 const App = () => {
   return (
     <>
       <Header/>
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Home/>
         </Container>
       </main>
       <Footer/>
