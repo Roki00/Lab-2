311438a5c39a9bbed64b6f1514eb27f56a1b8743
Eron <eb47836@ubt-uni.net>
2023-08-12 17:16:16 +0200

    Shtimi i react-router-domit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from "./components/header"
 import Footer from "./components/footer"
-import Home from './screens/home'
 const App = () => {
   return (
     <>
       <Header/>
       <main className='py-3'>
         <Container>
-          <Home/>
+            <Outlet/>
         </Container>
       </main>
       <Footer/>
