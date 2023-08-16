01f791c55bdee9bf0e1e013d69ee952c747336d3
Eron <eb47836@ubt-uni.net>
2023-08-17 00:16:46 +0200

    ju lutem e shkruni .js nfund kur boni importa

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 //this is the parent to our other API slices
 
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
+import { BASE_URL } from '../constants.js';
 
 const baseQuery = fetchBaseQuery ({baseUrl: BASE_URL});
 
