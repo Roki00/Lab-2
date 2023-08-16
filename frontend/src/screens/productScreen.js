3152ee50179dc814eee742c895331cd708dea335
lirika33 <la44091@ubt-uni.net>
2023-08-17 00:08:23 +0200

    endpoint

--- frontend/src/screens/productScreen.js
+++ frontend/src/screens/productScreen.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import{useState, useEffect} from 'react'
-import {useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { Row,Col,Image,ListGroup,Card,Button, ListGroupItem } from 'react-bootstrap'
-import Rating from '../components/rating'
-import axios from 'axios'
+import React from 'react';
+import {useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Row,Col,Image,ListGroup,Card,Button, ListGroupItem } from 'react-bootstrap';
+import Rating from '../components/rating';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice.js';
 
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState({});
-    
-    const {id: productId} = useParams();
+    const { id: productId} = useParams();
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-
-        fetchProduct();
-    },[productId]);
+    const { data: product, isLoading, error } = useGetProductDetailsQuery
+    (productId);
 
   return (
     <>
         <Link className='btn btn-light my-3' to="/">
             Go Back
         </Link>
-        <Row>
+
+        {isLoading ? (
+         <h2>Loading...</h2>
+        ): error ? (
+            <div>{ error?.data?.message || error.error }</div>
+        ) : (
+            <Row>
             <Col md={5}>
                 <Image src={product.image} alt={product.name} fluid/>
             </Col>
@@ -77,8 +74,10 @@ const ProductScreen = () => {
                 </Card>
             </Col>
         </Row>
+        )}
+        
     </>
-  )
-}
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
