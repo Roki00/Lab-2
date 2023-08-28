dc87b6444ad2d575efb0fbd630fb746056bd0eeb
Eron <eb47836@ubt-uni.net>
2023-08-28 04:47:30 +0200

    product edit screeni me error

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import  express from 'express';
-import { getProducts, getProductById, CreateProduct } from '../controllers/productController.js';
+import { getProducts, getProductById, CreateProduct, updateProduct } from '../controllers/productController.js';
 import {protect,admin} from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 router.route("/").get(getProducts).post(protect,admin,CreateProduct)
-router.route("/:id").get(getProductById)
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct)
 
 
 export default router
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,6 +28,7 @@ import ProfileScreen from './screens/ProfileScreen.js';
 import AdminRoute from "./components/AdminRoute.js"
 import OrderListScreen from "./screens/admin/OrderListScreen.js"
 import ProductListScreen from './screens/admin/ProductListScreen.js';
+import ProductEditScreen from './screens/admin/ProductEditScreen.js';
 
 
 const router = createBrowserRouter(
@@ -51,6 +52,7 @@ const router = createBrowserRouter(
         <Route path='' element={<AdminRoute/>}>
           <Route path='/admin/orderlist' element={<OrderListScreen/>} /> 
           <Route path='/admin/productlist' element={<ProductListScreen/>} /> 
+          <Route path='/admin/product/:id/edit' element={<ProductEditScreen/>}/>
         </Route> 
         
       </Route>
